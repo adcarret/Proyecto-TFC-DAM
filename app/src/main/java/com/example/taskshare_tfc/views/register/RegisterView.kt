851855656b6e2cec017dd21edccaf892a25e1b65
acarreteroal <acarreteroal@outlook.com>
2025-04-10 23:13:39 +0200

    add RegisterView function

--- app/src/main/java/com/example/taskshare_tfc/views/register/RegisterView.kt
+++ app/src/main/java/com/example/taskshare_tfc/views/register/RegisterView.kt
@@ -1,2 +1,12 @@
 package com.example.taskshare_tfc.views.register
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.taskshare_tfc.viewModels.RegisterViewModel
+
+@Composable
+
+fun RegisterView(navController: NavController, registerViewModel: RegisterViewModel){
+    Text(text = "Registro de usuario")
+}
