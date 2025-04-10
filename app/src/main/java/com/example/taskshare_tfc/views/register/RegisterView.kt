835b4ddefce98ae53ec166a81f5637709ebd3cc1
acarreteroal <acarreteroal@outlook.com>
2025-04-10 23:51:02 +0200

    RegisterView interface design

--- app/src/main/java/com/example/taskshare_tfc/views/register/RegisterView.kt
+++ app/src/main/java/com/example/taskshare_tfc/views/register/RegisterView.kt
@@ -1,12 +1,105 @@
 package com.example.taskshare_tfc.views.register
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.taskshare_tfc.R
 import com.example.taskshare_tfc.viewModels.RegisterViewModel
 
 @Composable
-
+//Vista de la pantalla de Registro de los usuarios
 fun RegisterView(navController: NavController, registerViewModel: RegisterViewModel){
-    Text(text = "Registro de usuario")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ){
+        var email by remember { mutableStateOf("") } //Inicilizo la variable email
+        var password by remember { mutableStateOf("") }//Inicializo la variable password
+        var username by remember { mutableStateOf("") }//Inicializo la variable username
+
+        //Titulo
+        Text(
+            modifier = Modifier.padding(top = 10.dp),
+            text = "Registro de usuario",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+
+        //Icono de registro
+        Image(
+            modifier = Modifier
+                .height(150.dp)
+                .width(150.dp)
+                .padding(15.dp),
+            painter = painterResource(id = R.drawable.iconoregistrodos),
+            contentDescription = "Logo de registro"
+        )
+
+        //Campo username
+        OutlinedTextField(
+            value = username,
+            onValueChange = {username = it},
+            label = { Text(text = "Username") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp))
+
+        //Campo email
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = { Text(text = "Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp))
+
+        //Campo password
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp))
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Botón de registro
+        Button(onClick = {/*TODO*/},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp)
+        ) {
+            Text(text = "Registrarme")
+        }
+    }
 }
