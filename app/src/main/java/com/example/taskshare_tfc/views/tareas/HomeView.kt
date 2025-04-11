29fae7e0dd9e0c05f68baf32a89be855bf4c134e
acarreteroal <acarreteroal@outlook.com>
2025-04-11 17:20:27 +0200

    create function HomeView only with one line Text

--- app/src/main/java/com/example/taskshare_tfc/views/tareas/HomeView.kt
+++ app/src/main/java/com/example/taskshare_tfc/views/tareas/HomeView.kt
@@ -1,2 +1,11 @@
 package com.example.taskshare_tfc.views.tareas
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.taskshare_tfc.viewModels.TareasViewModel
+
+@Composable
+fun HomeView(navController: NavController, notesVM : TareasViewModel){
+    Text(text = "Inicio")
+}
