512547519d7ab48424f70c7724e6bf855aa9b839
acarreteroal <acarreteroal@outlook.com>
2025-04-11 17:54:17 +0200

    create function login()

--- app/src/main/java/com/example/taskshare_tfc/viewModels/LoginViewModel.kt
+++ app/src/main/java/com/example/taskshare_tfc/viewModels/LoginViewModel.kt
@@ -1,6 +1,32 @@
 package com.example.taskshare_tfc.viewModels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
+
+    private val auth : FirebaseAuth = Firebase.auth
+
+    fun login(email : String, password : String, onSuccess : () -> Unit){
+        viewModelScope.launch {
+            try {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {task ->
+                        if(task.isSuccessful){
+                            onSuccess()
+                        }
+                    }
+                    .addOnFailureListener {
+                        Log.d("Error", "No se pudo iniciar sesión")
+                    }
+            }catch (e:Exception){
+                Log.d("Error", "${e.message}")
+            }
+        }
+    }
 }
