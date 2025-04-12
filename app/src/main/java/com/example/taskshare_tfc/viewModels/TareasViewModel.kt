f3090644e0e15d4b0ce0fa4c1c3f9db9f7216435
acarreteroal <acarreteroal@outlook.com>
2025-04-12 15:45:43 +0200

    addd function logout

--- app/src/main/java/com/example/taskshare_tfc/viewModels/TareasViewModel.kt
+++ app/src/main/java/com/example/taskshare_tfc/viewModels/TareasViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.taskshare_tfc.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class TareasViewModel : ViewModel() {
+    private val auth : FirebaseAuth = Firebase.auth
+
+    fun logOut(){
+        auth.signOut()//Nos permite cerrar la sesión
+    }
 }
