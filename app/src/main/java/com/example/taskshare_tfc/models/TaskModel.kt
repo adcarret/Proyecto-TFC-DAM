57eb5329aaf041f5c75787f3132d2e78292c6543
acarreteroal <acarreteroal@outlook.com>
2025-04-16 15:41:56 +0200

    creation taskmodel

---
+++ app/src/main/java/com/example/taskshare_tfc/models/TaskModel.kt
@@ -0,0 +1,10 @@
+package com.example.taskshare_tfc.models
+
+data class TaskModel(
+    val title : String = "",
+    val description : String = "",
+    val email : String = "",
+    val date: String = "",
+    val idTask : String = ""
+
+)
