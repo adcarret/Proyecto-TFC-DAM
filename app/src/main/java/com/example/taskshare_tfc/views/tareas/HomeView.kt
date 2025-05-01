1c0de82be87041a2e84275ae4474d5d26148de39
acarreteroal <acarreteroal@outlook.com>
2025-05-01 11:37:58 +0200

    added two images and buttons add contact

--- app/src/main/java/com/example/taskshare_tfc/views/tareas/HomeView.kt
+++ app/src/main/java/com/example/taskshare_tfc/views/tareas/HomeView.kt
@@ -1,8 +1,11 @@
 package com.example.taskshare_tfc.views.tareas
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material3.Button
@@ -15,8 +18,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.taskshare_tfc.R
 import com.example.taskshare_tfc.viewModels.TareasViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,6 +45,18 @@ fun HomeView(navController: NavController, notesVM : TareasViewModel){
     ){padding ->
         Column(modifier = Modifier.padding(padding),
             horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Image(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp)
+                    .padding(15.dp),
+                painter = painterResource(id = R.drawable.ico_tarea),
+                contentDescription = "Icono de tarea"
+
+            )
+
+
             //Botón agregar una tarea
             Button(onClick = {
                 navController.navigate("AddTask")
@@ -60,6 +77,28 @@ fun HomeView(navController: NavController, notesVM : TareasViewModel){
                     .padding(start = 20.dp, end = 20.dp)) {
                 Text(text = "Ver mis tareas")
 
+            }
+
+            Image(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp)
+                    .padding(15.dp),
+                painter = painterResource(id = R.drawable.ico_contacto),
+                contentDescription = "Icono de contacto"
+
+            )
+
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp)) {
+
+                Text(text = "Agregar contacto")
+            }
+
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp)) {
+
+                Text(text = "Ver mis contactos")
             }
 
         }
