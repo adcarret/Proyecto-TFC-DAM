4a49014ab282f206b338aed364efdbcb52cf56a7
acarreteroal <acarreteroal@outlook.com>
2025-05-07 18:11:55 +0200

    added a logic of button addcontact

--- app/src/main/java/com/example/taskshare_tfc/views/contacts/AaddContactsView.kt
+++ app/src/main/java/com/example/taskshare_tfc/views/contacts/AaddContactsView.kt
@@ -1,5 +1,6 @@
 package com.example.taskshare_tfc.views.contacts
 
+import android.widget.Toast
 import androidx.compose.animation.core.rememberTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
@@ -26,20 +27,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.taskshare_tfc.R
+import com.example.taskshare_tfc.viewModels.ContactsViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
-fun AddContactsView(){
+fun AddContactsView(navController: NavController, contactsViewModel: ContactsViewModel){
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "Agregar contacto") },
             navigationIcon = {
                 IconButton(onClick = {
-
+                    navController.popBackStack()
                 }) {
                     Icon(
                         imageVector = Icons.AutoMirrored.Filled.ArrowBack,
@@ -57,6 +61,7 @@ fun AddContactsView(){
             var email by remember { mutableStateOf("") }
             var address by remember { mutableStateOf("") }
             var phone by remember { mutableStateOf("") }
+            var context = LocalContext.current
 
             Image(
                 modifier = Modifier
@@ -112,7 +117,25 @@ fun AddContactsView(){
             /*Botón para agregar un contacto*/
             Button(
                 onClick ={
-
+                    when {
+                        names.isBlank() || email.isBlank() || address.isBlank() || phone.isBlank() -> {
+                            Toast.makeText(context, "Rellena todos los campos", Toast.LENGTH_SHORT).show()
+                        }
+                        !email.contains("@") -> {
+                            Toast.makeText(context, "Introduce un correo válido", Toast.LENGTH_SHORT).show()
+                        }
+                        !phone.all { it.isDigit() } -> {
+                            Toast.makeText(context, "El teléfono debe contener solo números", Toast.LENGTH_SHORT).show()
+                        }
+                        else -> {
+                            contactsViewModel.saveContact(
+                                names, email, address, phone
+                            ) {
+                                navController.popBackStack()
+                                Toast.makeText(context, "Contacto guardado", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
                 },
                 modifier = Modifier
                     .fillMaxWidth()
