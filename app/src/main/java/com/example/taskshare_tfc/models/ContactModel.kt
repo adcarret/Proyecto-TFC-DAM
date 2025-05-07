e70445acd5b34ac16f08ba04f36c03acd66dfa14
acarreteroal <acarreteroal@outlook.com>
2025-05-07 18:13:04 +0200

    create data class contactmodel

---
+++ app/src/main/java/com/example/taskshare_tfc/models/ContactModel.kt
@@ -0,0 +1,10 @@
+package com.example.taskshare_tfc.models
+
+data class ContactModel(
+    val myEmail : String = "", //Correo del usuario el que realiza el registro del contacto
+    val names : String = "",
+    val email : String = "", //Correo del contacto
+    val address : String = "",
+    val phone : String = ""
+
+)
