dfefea11c2947538ab7e18c9e4d863697e3c8ef7
acarreteroal <acarreteroal@outlook.com>
2025-05-08 17:51:11 +0200

    create class AllContactView

---
+++ app/src/main/java/com/example/taskshare_tfc/views/contacts/AllContactView.kt
@@ -0,0 +1,66 @@
+package com.example.taskshare_tfc.views.contacts
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.taskshare_tfc.viewModels.ContactsViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun AllContactsView(navController: NavController, contactVM : ContactsViewModel){
+
+    LaunchedEffect(Unit) {
+        contactVM.getContacts()
+
+    }
+
+
+
+    Scaffold (
+        topBar = {
+            TopAppBar(title =  { Text(text = "Mis contactos") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "")
+                    }
+                })
+        }
+    ){paddingValues ->
+        Column (
+            modifier = Modifier.padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            val contact by contactVM.contactData.collectAsState()
+
+            LazyColumn {
+                items(contact){item ->
+                    Text(text = item.names)
+                    Text(text = item.email)
+                    Text(text = item.address)
+                    Text(text = item.phone)
+
+                }
+            }
+        }
+    }
+}
